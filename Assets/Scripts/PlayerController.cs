01381593ddc175f108a80123a6c27fb954a404e3
bheifetz <bheifetz94@gmail.com>
2023-10-14 16:48:51 -0700

    Lesson D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,19 @@ public class PlayerController : MonoBehaviour
     public bool gameOver = false;
 
     private Animator animPlayer;
+    public ParticleSystem explosionSystem;
+    public ParticleSystem dust;
+
+    private AudioSource audioPlayer;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
         animPlayer = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -30,19 +37,27 @@ public class PlayerController : MonoBehaviour
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
             animPlayer.SetTrigger("Jump_trig");
+            dust.Stop();
+            audioPlayer.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
+        {
             onGround = true;
+            dust.Play();
+        }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game over");
             gameOver = true;
             animPlayer.SetBool("Death_b", true);
             animPlayer.SetInteger("DeathType_int", 2);
+            explosionSystem.Play();
+            dust.Stop();
+            audioPlayer.PlayOneShot(crashSound, 0.5f);
         }
     }
 }
